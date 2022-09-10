544b076d822d65ca1b13847534d97cb6059d1529
XiaoJiang0208 <jjm1837080396@gmail.com>
2022-09-10 22:27:13 +0800

    add number keyboard

--- 数字炸弹ProMax.py
+++ 数字炸弹ProMax.py
@@ -4,8 +4,6 @@ python编程复习(毕生所学doge)
 '''
 
 #导入gui库
-from cProfile import label
-from socket import BTPROTO_RFCOMM
 from tkinter import *
 
 #create the mainWindow
@@ -22,19 +20,66 @@ def num1():
     pass
     
 def drawMainWindow():
-    '''draw mainWindow'''
-    lb1 = Label(mainWindow, text="数字炸弹ProMax",font=("",50), justify=CENTER)
-    lb1.pack(side=TOP)
-    
-    #add img
     global photo
     photo = PhotoImage(file="c4.gif")
-    imgLb1 = Label(mainWindow,image=photo)
-    imgLb1.pack(expand=True, fill=BOTH, side=BOTTOM)
+    global photoLabel
+    photoLabel = Label(mainWindow, image=photo).pack()
     
-    #add number button
-    btMun1 = Button(imgLb1,text="1", command=num1).pack(pady=(375,0), ipadx=4)
-    
+    #add number keyBoark
+    global keyBoard
+    keyBoard = Frame(photoLabel).pack()
+    #1,2,3
+    global key123
+    key123 = Frame(keyBoard)
+    key123.pack()
+    global but1
+    but1 = Button(key123, text="1")
+    but1.pack(side=LEFT, padx=5, pady=5, ipadx=5)
+    global but2
+    but2 = Button(key123, text="2")
+    but2.pack(side=LEFT, padx=5, pady=5, ipadx=5)
+    global but3
+    but3 = Button(key123, text="3")
+    but3.pack(side=LEFT, padx=5, pady=5, ipadx=5)
+    #4,5,6
+    global key456
+    key456 = Frame(keyBoard)
+    key456.pack()
+    global but4
+    but4 = Button(key456, text="4")
+    but4.pack(side=LEFT, padx=5, pady=5, ipadx=5)
+    global but5
+    but5 = Button(key456, text="5")
+    but5.pack(side=LEFT, padx=5, pady=5, ipadx=5)
+    global but6
+    but6 = Button(key456, text="6")
+    but6.pack(side=LEFT, padx=5, pady=5, ipadx=5)
+    #7,8,9
+    global key789
+    key789 = Frame(keyBoard)
+    key789.pack()
+    global but7
+    but7 = Button(key789, text="4")
+    but7.pack(side=LEFT, padx=5, pady=5, ipadx=5)
+    global but8
+    but8 = Button(key789, text="5")
+    but8.pack(side=LEFT, padx=5, pady=5, ipadx=5)
+    global but9
+    but9 = Button(key789, text="6")
+    but9.pack(side=LEFT, padx=5, pady=5, ipadx=5)
+    #del,0,ok
+    global keyD0O
+    keyD0O = Frame(keyBoard)
+    keyD0O.pack()
+    global butDel
+    butDel = Button(keyD0O, text="*")
+    butDel.pack(side=LEFT, padx=5, pady=5, ipadx=6)
+    global but0
+    but0 = Button(keyD0O, text="0")
+    but0.pack(side=LEFT, padx=5, pady=5, ipadx=5)
+    global butOK
+    butOK = Button(keyD0O, text="#")
+    butOK.pack(side=LEFT, padx=5, pady=5, ipadx=5)
     
 if __name__ == "__main__":
     initMainWindow()
